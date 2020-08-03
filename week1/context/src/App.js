b6bb622a93bdc8b5f74d198af99636869c947661
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-08-03 12:05:55 -0500

    context

---
+++ week1/context/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import './App.css';
+import Header from "./Header"
+import Button from "./Button"
+import ThemeContext from "./themeContext"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <ThemeContext.Consumer>
+                {theme => (
+                    <Button theme={theme} />
+                )}
+            </ThemeContext.Consumer>
+            
+            <Button theme="light"/>
+        </div>
+    )
+}
+
+export default App
